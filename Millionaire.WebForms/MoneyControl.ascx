5138f332395350371a6a319df3c40cf3ee7de2ef
Das-Tier <vitalijmogola@gmail.com>
2015-06-27 01:43:59 +0300

    last

---
+++ Millionaire.WebForms/MoneyControl.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="MoneyControl.ascx.cs" Inherits="Millionaire.WebForms.MoneyControl" %>
+<p>Прогрес</p>
+<asp:Label ID="lbl_money" runat="server" />
+<asp:Label ID="lbl_unburned" runat="server" />
---
+++ Millionaire.WebForms/MoneyControl.ascx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Millionaire.WebForms
+{
+    public partial class MoneyControl : System.Web.UI.UserControl
+    {
+        public int Money { get; set; }
+        public int UnburnedMoney { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
